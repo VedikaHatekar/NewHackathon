26e122869e0289990633d43e4e5b39ffd4aaaae2
VedikaHatekar <hatekarv99@gmail.com>
2021-09-23 12:08:57 +0530

    added new components

--- newhack/src/app/app-routing.module.ts
+++ newhack/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PlayerComponent } from './player/player.component';
+import { TeamComponent } from './team/team.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'team-component', component: TeamComponent},
+  { path: 'player-component', component: PlayerComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
